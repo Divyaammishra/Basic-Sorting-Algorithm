748e88a3bbe9dac6b3d669b8753ee1cfd008f2c7
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2023-07-24 22:04:24 +0530

    Add files via upload

---
+++ BubbleSort/src/Main.java
@@ -0,0 +1,34 @@
+
+
+//Use Bubble Sort sorting algorithms to sort an array in DESCENDING order
+
+public class Main {
+
+    public static void bubbleSort(int arr[]){
+
+        for(int i=0; i<arr.length-2; i++){
+            for(int j=0; j<arr.length-1; j++){
+
+                if(arr[j] < arr[j+1]){
+
+                    int temp = arr[j];
+                     arr[j] = arr[j+1];
+                     arr[j+1] = temp;
+
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int arr[] = {3,6,2,1,8,7,4,5,3,1};
+
+        bubbleSort(arr);
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
